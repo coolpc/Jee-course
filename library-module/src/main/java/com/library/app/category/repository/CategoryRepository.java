cec6d4d86a6dd0d2635f685ca1f30d8f9cc7ad01
pramod chadda <pramod.chadda@dematic.com>
2018-05-13 21:10:33 -0400

    Manual Test 1
    Manual Test 1

--- library-module/src/main/java/com/library/app/category/repository/CategoryRepository.java
+++ library-module/src/main/java/com/library/app/category/repository/CategoryRepository.java
@@ -3,12 +3,14 @@ package com.library.app.category.repository;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import com.library.app.category.model.Category;
 
 public class CategoryRepository {
 
+	@PersistenceContext
 	EntityManager em;
 
 	public Category add(final Category category) {
--- library-resource/src/main/java/com/library/app/common/model/HttpCode.java
+++ library-resource/src/main/java/com/library/app/common/model/HttpCode.java
@@ -2,7 +2,9 @@ package com.library.app.common.model;
 
 public enum HttpCode {
     CREATED(201),
-    VALIDATION_ERROR(422);
+    VALIDATION_ERROR(422),
+    OK(200),
+    NOT_FOUND(404);
 
     private int code;
 
